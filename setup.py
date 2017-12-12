c5a5fa027fae2f307688adb42306257aaa39fdf4
Andrew Low <andrew.low@inspection.gc.ca>
2017-12-12 08:54:27 -0500

    Updated bbmap - Travis starting to have java issues?

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OLCTools",
-    version="0.3.16",
+    version="0.3.22",
     packages=find_packages(),
     author="Andrew Low",
     author_email="andrew.low@inspection.gc.ca",
