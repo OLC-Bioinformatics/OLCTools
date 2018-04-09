a744afbb2039e38de5255f10ab3234ab557edcb5
adamkoziol <adam.koziol@inspection.gc.ca>
2018-04-09 14:30:20 -0400

    Incorporated error rate and % reads mapping to PhiX controls. Adjusted reports accordingly.

---
+++ spadespipeline/phix.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python 3
+from accessoryFunctions.accessoryFunctions import printtime
+from interop import py_interop_run_metrics, py_interop_run, py_interop_summary
+import os
+
+__author__ = 'adamkoziol'
+
+
+class PhiX(object):
+
+    def main(self):
+        printtime('Attempting to extract PhiX mapping data', self.start)
+        interop_folder = os.path.join(self.path, 'InterOp')
+        # Determine if the InterOp folder is present
+        if os.path.isdir(interop_folder):
+            # Extract the relevant information
+            self.interop_parse()
+        else:
+            # Create attributes reflecting the lack of the InterOp folder
+            for sample in self.metadata:
+                sample.run.error_rate = 'NA'
+                sample.run.phix_aligned = 'NA'
+
+    def interop_parse(self):
+        """
+        Use interop to parse the files in the InterOp folder to extract the number of reads mapping to PhiX as well as
+        the error rate
+        """
+        # Parse the files and load the data
+        run_metrics = py_interop_run_metrics.run_metrics()
+        valid_to_load = py_interop_run.uchar_vector(py_interop_run.MetricCount, 0)
+        py_interop_run_metrics.list_summary_metrics_to_load(valid_to_load)
+        run_metrics.read(self.path, valid_to_load)
+        summary = py_interop_summary.run_summary()
+        py_interop_summary.summarize_run_metrics(run_metrics, summary)
+        # PhiX error rate for run over all "usable cycles"
+        errorrate = summary.total_summary().error_rate()
+        # Percent aligned PhiX
+        pctaligned = summary.total_summary().percent_aligned()
+        # Add the error rate and the percent of reads that align to PhiX to the metadata object
+        for sample in self.metadata:
+            sample.run.error_rate = '{:.2f}'.format(errorrate)
+            sample.run.phix_aligned = '{:.2f}'.format(pctaligned)
+
+    def __init__(self, inputobject):
+        self.path = inputobject.path
+        self.start = inputobject.starttime
+        self.metadata = inputobject.runmetadata.samples
