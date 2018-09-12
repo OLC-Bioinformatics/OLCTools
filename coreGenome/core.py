c775004b8ea34ee1d2645087989b404d5816febe
Adam Koziol <adam.koziol@inspection.gc.ca>
2018-09-12 10:59:48 -0400

    Modified core.py to work with new version of geneseekr

--- coreGenome/core.py
+++ coreGenome/core.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from accessoryFunctions.accessoryFunctions import MetadataObject, printtime
-from spadespipeline.GeneSeekr import GeneSeekr
+from geneseekr.geneseekr import BLAST
 from Bio import SeqIO
 from csv import DictReader
 from glob import glob
@@ -10,105 +10,124 @@ import os
 __author__ = 'adamkoziol'
 
 
-class CoreGenome(GeneSeekr):
+class CoreGenome(BLAST):
 
-    def blastparser(self, report, sample):
-        # Open the sequence profile file as a dictionary
-        blastdict = DictReader(open(report), fieldnames=self.fieldnames, dialect='excel-tab')
-        resultdict = dict()
-        coregenomes = list()
-        # Create a list of all the names of the database files - glob, replace - with _, remove path and extension
-        corefiles = glob(
-            os.path.join(self.referencefilepath, self.analysistype, sample.general.referencegenus, '*.tfa'))
-        for fasta in corefiles:
-            fastaname = os.path.basename(os.path.splitext(fasta)[0]).replace('-', '_')
-            fastaname = fastaname.split('.')[0]
-            coregenomes.append(fastaname)
-        # Initialise resultdict with an integer for every database file
-        for genome in coregenomes:
-            resultdict[genome] = int()
-        # A set to store the number of core genes
-        coregenes = set()
-        # Go through each BLAST result
-        for row in blastdict:
-            # Calculate the percent identity and extract the bitscore from the row
-            # Percent identity is the (length of the alignment - number of mismatches) / total subject length
-            # noinspection PyTypeChecker
-            percentidentity = float('{:0.2f}'.format((float(row['positives']) - float(row['gaps'])) /
-                                                     float(row['subject_length']) * 100))
-            # Split off any | from the sample name
-            target = row['subject_id'].split('|')[0]
-            # As there are variable numbers of _ in the name, try to split off the last one only if there are multiple
-            # and only keep the first part of the split if there is one _ in the name
-            underscored = '_'.join(target.split('_')[:-1]) if len(target.split('_')) > 2 else target.split('_')[0]
-            try:
-                # Since the number of core genes is the same for each reference strain, only need to determine it once
-                if underscored == sorted(coregenomes)[0]:
-                    coregenes.add(target)
-                # If the percent identity is greater than the cutoff - adjust the cutoff to 90% for these analyses
-                self.cutoff = 90
-                if percentidentity >= self.cutoff:
-                    # Update the dictionary with the target and the number of hits
-                    resultdict[underscored] += 1
-            except (KeyError, IndexError):
-                pass
-        # Sort the dictionary on the number of hits - best at the top
-        topcore = sorted(resultdict.items(), key=operator.itemgetter(1), reverse=True)
-        # Initialise a dictionary attribute to store results
-        sample[self.analysistype].blastresults = dict()
-        # If there are no results, populate negative results
-        if not resultdict:
-            sample[self.analysistype].blastresults = 'NA'
-        # If results, add a string of the best number of hits, and a string of the total number of genes
-        else:
-            sample[self.analysistype].blastresults[topcore[0][0]] = (str(topcore[0][1]), str(len(coregenes)))
+    def parse_results(self):
+        self.parser(self.metadata,
+                    self.analysistype,
+                    self.fieldnames,
+                    self.cutoff,
+                    self.program)
 
-    def reporter(self):
+    @staticmethod
+    def parser(metadata, analysistype, fieldnames, cutoff, program):
+        for sample in metadata:
+            # Open the sequence profile file as a dictionary
+            blastdict = DictReader(open(sample[analysistype].report), fieldnames=fieldnames, dialect='excel-tab')
+            resultdict = dict()
+            # Initialise a dictionary to store all the target sequences
+            sample[analysistype].targetsequence = dict()
+            coregenomes = list()
+            # A set to store the number of core genes
+            coregenes = set()
+            # Create a list of all the names of the database files - glob, replace - with _, remove path and extension
+            for fasta in sample[analysistype].targets:
+                fastaname = os.path.basename(os.path.splitext(fasta)[0]).replace('-', '_')
+                fastaname = fastaname.split('.')[0]
+                coregenomes.append(fastaname)
+            # Go through each BLAST result
+            for row in blastdict:
+                # Create the subject length variable - if the sequences are DNA (e.g. blastn), use the subject
+                # length as usual; if the sequences are protein (e.g. tblastx), use the subject length / 3
+                if program == 'blastn' or program == 'blastp' or program == 'blastx':
+                    subject_length = float(row['subject_length'])
+
+                else:
+                    subject_length = float(row['subject_length']) / 3
+                # Calculate the percent identity and extract the bitscore from the row
+                # Percent identity is the (length of the alignment - number of mismatches) / total subject length
+                percentidentity = float('{:0.2f}'.format((float(row['positives']) - float(row['gaps'])) /
+                                                         subject_length * 100))
+                # If the percent identity is greater than the cutoff
+                if percentidentity >= cutoff:
+                    # Split off any | from the sample name
+                    target = row['subject_id'].split('|')[0]
+                    # As there are variable numbers of _ in the name, try to split off the last one only if there are
+                    #  multiple and only keep the first part of the split if there is one _ in the name
+                    underscored = '_'.join(target.split('_')[:-1]) if len(target.split('_')) > 2 else target.split('_')[
+                        0]
+                    try:
+                        # Since the number of core genes is the same for each reference strain,
+                        # only need to determine it once
+                        if underscored == sorted(coregenomes)[0]:
+                            coregenes.add(target)
+                        # Update the dictionary with the target and the number of hits
+                        resultdict[underscored] += 1
+                    except (KeyError, IndexError):
+                        pass
+                    # Sort the dictionary on the number of hits - best at the top
+                topcore = sorted(resultdict.items(), key=operator.itemgetter(1), reverse=True)
+                # Initialise a dictionary attribute to store results
+                sample[analysistype].blastresults = dict()
+                # If there are no results, populate negative results
+                if not resultdict:
+                    sample[analysistype].blastresults = 'NA'
+                # If results, add a string of the best number of hits, and a string of the total number of genes
+                else:
+                    sample[analysistype].blastresults[topcore[0][0]] = (str(topcore[0][1]), str(len(coregenes)))
+        return metadata
+
+    def create_reports(self):
+        # Create dictionaries
+        self.metadata = self.geneseekr.dict_initialise(self.metadata,
+                                                       self.analysistype)
+        self.reporter(self.metadata, self.analysistype, self.reportpath)
+
+    @staticmethod
+    def reporter(metadata, analysistype, reportpath):
         header = 'Strain,ClosestRef,GenesPresent/Total,\n'
         data = str()
-        for sample in self.metadata:
+        for sample in metadata:
             try:
-                if sample[self.analysistype].blastresults != 'NA':
+                if sample[analysistype].blastresults != 'NA':
                     if sample.general.closestrefseqgenus == 'Listeria':
                         # Write the sample name, closest ref genome, and the # of genes found / total # of genes
-                        closestref = list(sample[self.analysistype].blastresults.items())[0][0]
-                        coregenes = list(sample[self.analysistype].blastresults.items())[0][1][0]
+                        closestref = list(sample[analysistype].blastresults.items())[0][0]
+                        coregenes = list(sample[analysistype].blastresults.items())[0][1][0]
                         # Find the closest reference file
                         try:
-                            ref = glob(os.path.join(self.referencefilepath, self.analysistype, 'Listeria', '{fasta}*'
+                            ref = glob(os.path.join(sample[analysistype].targetpath, '{fasta}*'
                                                     .format(fasta=closestref)))[0]
                         except IndexError:
                             # Replace underscores with dashes to find files
                             closestref = closestref.replace('_', '-')
-                            ref = glob(os.path.join(self.referencefilepath, self.analysistype, 'Listeria', '{fasta}*'
+                            ref = glob(os.path.join(sample[analysistype].targetpath, '{fasta}*'
                                                     .format(fasta=closestref)))[0]
                         # Determine the number of core genes present in the closest reference file
                         totalcore = 0
                         for _ in SeqIO.parse(ref, 'fasta'):
                             totalcore += 1
                         # Add the data to the object
-                        sample[self.analysistype].targetspresent = coregenes
-                        sample[self.analysistype].totaltargets = totalcore
-                        sample[self.analysistype].coreresults = '{}/{}'.format(coregenes, totalcore)
+                        sample[analysistype].targetspresent = coregenes
+                        sample[analysistype].totaltargets = totalcore
+                        sample[analysistype].coreresults = '{}/{}'.format(coregenes, totalcore)
                         row = '{},{},{}/{}\n'.format(sample.name, closestref, coregenes, totalcore)
-                        # If the script is being run as part of the assembly pipeline, make a report for each sample
-                        if self.pipeline:
-                            # Open the report
-                            with open(os.path.join(sample[self.analysistype].reportdir,
-                                                   '{}_{}.csv'.format(sample.name, self.analysistype)), 'w') as report:
-                                # Write the row to the report
-                                report.write(header)
-                                report.write(row)
+                        # Open the report
+                        with open(os.path.join(sample[analysistype].reportdir,
+                                               '{}_{}.csv'.format(sample.name, analysistype)), 'w') as report:
+                            # Write the row to the report
+                            report.write(header)
+                            report.write(row)
                         data += row
                     else:
-                        sample[self.analysistype].targetspresent = 'NA'
-                        sample[self.analysistype].totaltargets = 'NA'
-                        sample[self.analysistype].coreresults = 'NA'
+                        sample[analysistype].targetspresent = 'NA'
+                        sample[analysistype].totaltargets = 'NA'
+                        sample[analysistype].coreresults = 'NA'
             except KeyError:
-                sample[self.analysistype].targetspresent = 'NA'
-                sample[self.analysistype].totaltargets = 'NA'
-                sample[self.analysistype].coreresults = 'NA'
-        with open(os.path.join(self.reportpath, 'coregenome.csv'), 'w') as report:
+                sample[analysistype].targetspresent = 'NA'
+                sample[analysistype].totaltargets = 'NA'
+                sample[analysistype].coreresults = 'NA'
+        with open(os.path.join(reportpath, 'coregenome.csv'), 'w') as report:
             # Write the data to the report
             report.write(header)
             report.write(data)
@@ -190,7 +209,7 @@ class AnnotatedCore(object):
             except KeyError:
                 pass
 
-    def __init__(self, inputobject):
+    def __init__(self, inputobject, genus_specific=False):
         self.start = inputobject.starttime
         self.commit = inputobject.commit
         self.starttime = inputobject.starttime
@@ -211,5 +230,6 @@ class AnnotatedCore(object):
                            'evalue', 'bit_score', 'subject_length', 'alignment_length',
                            'query_start', 'query_end', 'query_sequence',
                            'subject_start', 'subject_end', 'subject_sequence']
+        self.genus_specific = genus_specific
         # Run the analyses
         self.annotatedcore()
