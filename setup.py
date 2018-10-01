10d806e111264e3e0e0f1f2f64473917dbae1b2c
Adam Koziol <adam.koziol@inspection.gc.ca>
2018-10-01 10:35:48 -0400

    Updates to versions

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="OLCTools",
-    version="0.4.7",
+    version="0.4.8",
     packages=find_packages(),
     include_package_data=True,
     author="Andrew Low",
-    author_email="andrew.low@inspection.gc.ca",
+    author_email="andrew.low@canada.ca",
     url="https://github.com/lowandrew/OLCTools",
-    install_requires=['biopython', 'interop']
+    install_requires=['biopython', 'interop', 'rauth', 'click']
 )
