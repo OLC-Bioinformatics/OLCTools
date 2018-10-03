d00921540121f4d5bfb489c81b35bb8f87f1262e
Adam Koziol <adam.koziol@inspection.gc.ca>
2018-10-03 14:58:48 -0400

    Updated pip and conda packaging information

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OLCTools",
-    version="0.4.9",
+    version="0.5.0",
     packages=find_packages(),
     include_package_data=True,
     author="Andrew Low",
