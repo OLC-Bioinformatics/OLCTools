01f34aa46efdc1e0e8ff097f8b30cd566a55c8a9
adamkoziol <adam.koziol@inspection.gc.ca>
2018-11-05 11:05:38 -0500

    Updates to packaging

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OLCTools",
-    version="0.5.9",
+    version="0.6.0",
     packages=find_packages(),
     include_package_data=True,
     author="Andrew Low",
