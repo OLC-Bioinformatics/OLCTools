e8481a74ada1f156e466a9c805227e716109e084
lowandrew <andrew.low@canada.ca>
2019-02-20 10:23:00 -0500

    Update to version 0.8.3 - mob_suite now run in parallel for big time savings. Means upgrade to mob_suite 1.4.9.1 is necessary

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OLCTools",
-    version="0.8.2",
+    version="0.8.3",
     packages=find_packages(),
     include_package_data=True,
     author="Andrew Low",
--- spadespipeline/mobrecon.py
+++ spadespipeline/mobrecon.py
@@ -36,7 +36,6 @@ class MobRecon(object):
                 sample[self.analysistype].logerr = os.path.join(sample[self.analysistype].outputdir, 'err')
                 make_path(sample[self.analysistype].outputdir)
                 if sample.general.bestassemblyfile != 'NA':
-                    # Create the system call TODO: Run these instances in parallel.
                     sample.commands.mobrecon = 'mob_recon -i {fasta} -o {outdir} --run_typer -n {threads} -d {databases}'\
                         .format(fasta=sample.general.bestassemblyfile,
                                 outdir=sample[self.analysistype].outputdir,
